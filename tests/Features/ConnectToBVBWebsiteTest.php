9e5e5160a3cd625f6cbffac514e6f56af8c59c76
Mihai <48624094+mihaichris@users.noreply.github.com>
2022-02-04 19:17:53 +0200

    added test for the feature file

---
+++ tests/Features/ConnectToBVBWebsiteTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BVB;
+
+use PHPUnit\Framework\TestCase;
+
+class ConnectToBVBWebsiteTest extends TestCase
+{
+    public function testAccesingBVBWebsite()
+    {
+        $this->assertTrue(true);
+    }
+}
